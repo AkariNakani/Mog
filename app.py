8088cc72f01a452cdc64ab490289b96dcb3dd5a3
AkariNakani <119601138+AkariNakani@users.noreply.github.com>
2022-12-02 00:44:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://github.com/AkariNakani/Namui ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 -m nezuko &")
+os.system("git clone https://github.com/AkariNakani/Namui nami && cd nami && pip3 install -U -r requirements.txt && nohup python3 -m nezuko &")
